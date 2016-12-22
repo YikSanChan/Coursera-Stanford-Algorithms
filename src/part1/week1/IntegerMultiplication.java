d30370846092c20b0e18c297ba67a9da6e4c61db
Evan Chan <evan.chanyiksan@gmail.com>
2016-12-22 11:10:21 -0500

    initial commit

---
+++ src/part1/week1/IntegerMultiplication.java
@@ -0,0 +1,34 @@
+package part1.week1;
+
+import java.math.BigInteger;
+
+/**
+ * Created by CYS on 2016/12/22.
+ */
+public class IntegerMultiplication {
+
+    /**
+     * To test custom algorithm's correctness
+     */
+    public BigInteger bigIntegerMultiplication(BigInteger x, BigInteger y)
+    {
+        return x.multiply(y);
+    }
+
+    /**
+     * Third grade O(n^2) integer multiplication
+     * n = digits of x or y
+     */
+    public String thirdGrade(String x, String y)
+    {
+        return null;
+    }
+
+    /**
+     * Karatsuba multiplication
+     */
+    public String karatsuba(String x, String y)
+    {
+        return null;
+    }
+}
